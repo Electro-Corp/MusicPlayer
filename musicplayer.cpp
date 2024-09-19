07121b11d6992545d0eb47ed414b2a7d823a70ea
Electro-Corp <chinmaytanmay@outlook.com>
2024-09-18 18:13:47 -0600

    file looking

--- musicplayer.cpp
+++ musicplayer.cpp
@@ -6,6 +6,30 @@ MusicPlayer::MusicPlayer(QWidget *parent)
     , ui(new Ui::MusicPlayer)
 {
     ui->setupUi(this);
+
+    // Start finding music files
+    musicLibraryPath = "/home/segfault/Music";
+    loadMusic();
+
+}
+
+void MusicPlayer::loadMusic(){
+    //
+    printf("Loading music from %s\n", musicLibraryPath.c_str());
+    QStringListModel *model = new QStringListModel(this);
+    QStringList items;;
+    for (const auto& dirEntry : recurDir(musicLibraryPath.c_str())){
+        if(dirEntry.is_directory()){
+            printf("Hell nawww\n");
+        }else{
+            items.append((dirEntry.path().c_str()));
+        }
+
+    }
+    model->setStringList(items);
+    ui->songList->setModel(model);
+
+
 }
 
 MusicPlayer::~MusicPlayer()
--- musicplayer.h
+++ musicplayer.h
@@ -2,6 +2,11 @@
 #define MUSICPLAYER_H
 
 #include <QMainWindow>
+#include <QStringListModel>
+
+#include <filesystem>
+
+using recurDir = std::filesystem::recursive_directory_iterator;
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +22,12 @@ public:
     MusicPlayer(QWidget *parent = nullptr);
     ~MusicPlayer();
 
+
+    void loadMusic();
+
 private:
     Ui::MusicPlayer *ui;
+
+    std::string musicLibraryPath;
 };
 #endif // MUSICPLAYER_H
